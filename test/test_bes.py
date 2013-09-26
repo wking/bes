1de9a8ef308044750c244adfcb1d15a5949242ef
W. Trevor King <wking@tremily.us>
2013-09-25 19:48:03 -0700

    test/test_bes.py: Add EmitTestCase.test_emit

--- test/test_bes.py
+++ test/test_bes.py
@@ -15,3 +15,25 @@ class ConnectionTestCase (_unittest.TestCase):
         self.assertEqual(
             [msg for msg,addr in listener.messages],
             [b'Hello!', b'Goodbye!'])
+
+
+class EmitTestCase (_unittest.TestCase):
+    def _call_emit(self, *args, **kwargs):
+        with _udp_listener.UDPListener(count=1) as listener:
+            _bes.emit(
+                host=listener.host, port=listener.port, protocol='UDP',
+                *args, **kwargs)
+        self.assertEqual(len(listener.messages), 1)
+        self.assertEqual(len(listener.messages), 1)
+        return listener.messages[0][0]
+
+    def test_emit(self):
+        message = self._call_emit(
+            payload={'hello': 'world', 'goodbye': 'everybody'})
+        self.assertEqual(
+            message,
+            b'\n'.join([
+                b'{"index": {"_type": "record", "_index": "log"}}',
+                b'{"hello": "world", "goodbye": "everybody"}',
+                b'',
+            ]))
